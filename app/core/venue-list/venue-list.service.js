d7a01d7f761225d552858fe50cc903fe753b2eac
Michael Benson <michael.alexander.benson@gmail.com>
2017-05-16 16:34:28 -0400

    changed host

--- app/core/venue-list/venue-list.service.js
+++ app/core/venue-list/venue-list.service.js
@@ -2,7 +2,7 @@ angular.
 	module('core.venueList').
 		factory('VenueList', ['$resource',
 			function($resource) { 
-				return $resource('http://localhost:3000/venues.json', {}, {
+				return $resource('https://damp-gorge-42650.herokuapp.com/venues.json', {}, {
 					query: {
 						method: 'GET',
 						isArray: true
